0ef44343bbd392d7676e20a451843b31eefb5c96
Meirzhan14 <meirzhan-250@mail.ru>
2018-12-24 19:27:42 +0600

    added 46

--- src/Vk/autoboxing/CollectionMain.java
+++ src/Vk/autoboxing/CollectionMain.java
@@ -1,15 +1,28 @@
 package Vk.autoboxing;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class CollectionMain {
     public static void main(String[] args) {
-        List<Object> list = new LinkedList<>();
-        list.add(1);
+        List<Integer> list = new LinkedList<>();
+        list.add(Integer.valueOf(1));
 
-        for (int i = 0; i < list.size(); i++) {
-            String s = (String) list.get(1);
+        Integer integer = 56;
+        int n = integer.intValue();
+        Double doub = 56.55;
+        double d = doub.doubleValue();
+
+        String[] colors = {"yellow", "red", "white"};
+
+        LinkedList<String> li = new LinkedList<>(Arrays.asList(colors));
+        li.add("black");
+
+        colors = li.toArray(new String[li.size()]);
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
         }
     }
 }
---
+++ src/Vk/collectionsClass/CollectionsRunner.java
@@ -0,0 +1,56 @@
+package Vk.collectionsClass;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsRunner {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static class Card implements Comparable<Card>{
+
+
+        private enum Suit{SPADES, HEARTS, CLUBS, DIMONS}
+
+        private enum Face{ACE, DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NUNE, TEN, JACK, QUEEN, KING}
+
+        private final Suit suit;
+        private final Face face;
+
+        public Card(Suit suit, Face face) {
+            this.suit = suit;
+            this.face = face;
+        }
+
+        public Suit getSuit() {
+            return suit;
+        }
+
+        public Face getFace() {
+            return face;
+        }
+
+        @Override
+        public int compareTo(Card card) {
+
+            Face[] values = Face.values();
+            List<Face> faces = Arrays.asList(values);
+
+            if(faces.indexOf(this.face)<faces.indexOf(card.getFace())){
+                return -1;
+            } else if (faces.indexOf(this.face)>faces.indexOf(card.getFace())){
+                return +1;
+            } else if (faces.indexOf(this.face)==faces.indexOf(card.getFace())){
+                return String.valueOf(suit).compareTo(String.valueOf(card.getSuit()));
+            }
+            return 0;
+        }
+
+    }
+}
